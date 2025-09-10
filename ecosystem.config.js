692d6d288753569b3c0997b40a2a512b82238217
chetan.sharma.83@outlook.com <chetan sharma>
2025-09-10 08:36:00 +0530

    update in ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "smartapi-worker",
       script: "run.py",
       interpreter: "python3",
-      cwd: "/home/lenovo/Documents/PPersonal/MCD-project/backend/smartapi_candle_tracker",
+      cwd: "/opt/apps/TradeX-Worker",
       instances: 1,
       exec_mode: "fork",
       watch: false,
@@ -14,26 +14,28 @@ module.exports = {
       max_memory_restart: "512M",
       env: {
         ENV: "production",
-        BACKEND_BASE_URL: "http://localhost:3000",
-        BACKEND_WEBHOOK_URL: "http://localhost:3000/api/in-memory-candles",
+        BACKEND_BASE_URL: "http://159.89.171.16.sslip.io",
+        BACKEND_WEBHOOK_URL:
+          "http://159.89.171.16.sslip.io/api/in-memory-candles",
         WORKER_HOST: "0.0.0.0",
         WORKER_PORT: "5000",
-        LOG_LEVEL: "INFO"
+        LOG_LEVEL: "INFO",
       },
       env_development: {
         ENV: "development",
-        BACKEND_BASE_URL: "http://localhost:3000",
-        BACKEND_WEBHOOK_URL: "http://localhost:3000/api/in-memory-candles",
+        BACKEND_BASE_URL: "http://159.89.171.16.sslip.io",
+        BACKEND_WEBHOOK_URL:
+          "http://159.89.171.16.sslip.io/api/in-memory-candles",
         WORKER_HOST: "0.0.0.0",
         WORKER_PORT: "5000",
-        LOG_LEVEL: "DEBUG"
+        LOG_LEVEL: "DEBUG",
       },
       error_file: "./logs/smartapi-worker-error.log",
       out_file: "./logs/smartapi-worker-out.log",
       log_file: "./logs/smartapi-worker-combined.log",
       time: true,
       merge_logs: true,
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z"
-    }
-  ]
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+    },
+  ],
 };
